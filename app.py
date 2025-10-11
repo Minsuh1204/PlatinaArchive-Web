a3cacbc6d6a6c418a62e9e3f4999e540a521f1bb
Endeavy <mschang1204@naver.com>
2025-10-10 18:38:37 -0600

    Add sqlalchemy pool recycle

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("LAB_DB_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 3600
 app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_SECRET")
 app.register_blueprint(api_bp)
 
